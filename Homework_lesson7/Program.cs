ce1d8b083055a786d6cb92217b5e959c59425712
Eugenia <eugeniamail19@gmail.com>
2022-08-10 17:02:43 +0600

    homework for lesson 7, redone

--- Homework_lesson7/Program.cs
+++ Homework_lesson7/Program.cs
@@ -1,30 +1,43 @@
 ﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами, округлёнными до одного знака.
 
-/*
-Random rand = new Random();
-double[,] array = new double[4, 5];
+void Zadacha47()
+{
+    double[,] array = new double[4,5];
 
-for (int i = 0; i < array.GetLength(0); i ++)
+    FillArrayDouble(array);
+    PrintArrayDouble(array);
+}
+void FillArrayDouble(double[,] currentArray)
 {
-    for(int j = 0; j < array.GetLength(1); j++)
+    Random rand = new Random();
+    for (int i = 0; i < currentArray.GetLength(0); i++)
     {
-        array[i, j] = rand.NextDouble() * 10 - 5;
+        for(int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            currentArray[i, j] = Math.Round(rand.NextDouble() * 10 - 5, 2);
+        }
     }
 }
-for (int i = 0; i < array.GetLength(0); i++)
+
+void PrintArrayDouble(double[,] currentArray)
 {
-    for(int j = 0; j < array.GetLength(1); j++)
+        for (int i = 0; i < currentArray.GetLength(0); i++)
     {
-        Console.Write(array[i, j] + "\t");
+        for(int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            Console.Write(currentArray[i, j] + "\t");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
 }
-*/
+// Zadacha47
+
 
 // Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-/*
-
-    double[,] array = new double[4, 5];
+ 
+void Zadacha50()
+ {
+    double[,] array = new double[4,5];
     int row;
     int column;
     FillArrayDouble(array);
@@ -37,18 +50,24 @@ for (int i = 0; i < array.GetLength(0); i++)
     Console.WriteLine();
     if (row < array.GetLength(0) && column < array.GetLength(1))
     {
-        Console.WriteLine("Искомый элемент" + array[row, column]);
+        Console.WriteLine("Искомый элемент " + array[row-1, column-1]);
     }
-*/
-
+    else 
+    {
+        Console.WriteLine("Искомый элемент не найден");
+    }
+ }
+// Zadacha50();
 
 // Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
+void Zadacha52()
+{
     double[,] array = new double[4, 5];
-    // FillArrayDouble(array);
-    // PrintArrayDouble(array);
+    FillArrayDouble(array);
+    PrintArrayDouble(array);
 
-    for (int j =0; j < array.GetLength(1); j++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
         double sum = 0;
         for (int i = 0; i < array.GetLength(0); i++)
@@ -59,5 +78,5 @@ for (int i = 0; i < array.GetLength(0); i++)
         Console.WriteLine($"Для {j+1} столбца среднее арифметическое равно " + Math.Round(sum, 2));
         Console.WriteLine();
     }
-
-
+}
+Zadacha52();
